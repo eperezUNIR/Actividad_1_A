1c788a426e1bc1643b067a54ed33fd950476363a
eperezUNIR <153448721+eperezUNIR@users.noreply.github.com>
2023-12-10 23:47:04 +0100

    Update api_test.py

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -25,6 +25,16 @@ class TestApi(unittest.TestCase):
             response.read().decode(), "3", "ERROR ADD"
         )
 
+    def test_api_multiply(self):
+        url = f"{BASE_URL}/calc/multiply/3/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "6", "ERROR ADD"
+        )
+
     def test_api_sqrt(self):
         url = f"{BASE_URL_MOCK}/calc/sqrt/64"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
