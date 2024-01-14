d1be5faf636ab03941cd998a14215a815db19fa5
eperezUNIR <153448721+eperezUNIR@users.noreply.github.com>
2024-01-14 15:35:48 +0100

    Update calc.py

--- app/calc.py
+++ app/calc.py
@@ -19,7 +19,7 @@ class Calculator:
     def divide(self, x, y):
         self.check_types(x, y)
         if y == 0:
-            raise                 TypeError("Division by zero is not possible")
+            raise TypeError("Division by zero is not possible")
 
         return x / y
 
@@ -29,7 +29,7 @@ class Calculator:
 
     def check_types(self, x, y):
         if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
-            raise                      TypeError("Parameters must be numbers")
+            raise TypeError("Parameters must be numbers")
 
 if __name__ == "__main__":  # pragma: no cover
     calc = Calculator()
